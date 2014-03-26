e8ae519d64d8a8bde99c0b39c8e0773c3ca62690
Jim Craveiro <technicalfox@csh.rit.edu>
2014-03-25 20:46:36 -0400

    Working on having accounts for each user.
    Still setting up django and learning too...
    Started work on python file that will set up
    foxlatch for you on your pi.

--- foxlatch/urls.py
+++ foxlatch/urls.py
@@ -7,8 +7,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^admin/', include(admin.site.urls)),
     (r'^(?P<path>.*)$', 'django.views.static.serve',
             {'document_root': '/users/u21/technicalfox/Projects/FoxLatch/foxlatch/static/'}),
 
-    url(r'^admin/', include(admin.site.urls)),
 )
