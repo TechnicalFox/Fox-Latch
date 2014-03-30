a8cdf19f35558b36dfe7c45e4fc1769532ac7885
Jim Craveiro <technicalfox@csh.rit.edu>
2014-03-29 23:31:33 -0400

    Changed hall effect test.
    Made the test easier to understand, and
    for faster notification of change.

--- raspi/hallEffectTest.py
+++ raspi/hallEffectTest.py
@@ -5,11 +5,17 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+state = GPIO.input(11)
+if state == 0: print('Magnet detected.')
+else: print('No magnet detected.')
+
 while True:
     try:
-        #GPIO.wait_for_edge(12, GPIO.BOTH)
-        print(GPIO.input(11))
-        time.sleep(0.5)
+        curState = GPIO.input(11)
+        if curState != state:
+            if curState == 0: print('Magnet detected.')
+            else: print('No magnet detected.')
+        state = curState
     except KeyboardInterrupt:
         GPIO.cleanup()
         sys.exit()
